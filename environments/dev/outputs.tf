5c4a5413d430c7de29638302728f12feffae6596
Adi Wijaya <adiwijaya@ad.corp.google.com>
2020-02-01 16:58:52 +0800

    destroy all

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -11,16 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-
-output "network" {
-  value = "${module.vpc.network}"
-}
-
-output "subnet" {
-  value = "${module.vpc.subnet}"
-}
-
-output "firewall_rule" {
-  value = "${module.firewall.firewall_rule}"
-}
